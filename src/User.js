7432dfc5824c7649d8269abd95edca86c37795f6
AbdalrhmanBanyissa <aboood.banyissa.94@gmail.com>
2021-07-17 22:49:21 +0300

    authentication is complete

---
+++ src/User.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Card } from "react-bootstrap";
+
+const User = () => {
+  const { user, isAuthenticated } = useAuth0();
+
+  return (
+    isAuthenticated && (
+      <div>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={user.picture} alt={user.name} />
+          <Card.Body>
+            <Card.Title>{user.name}</Card.Title>
+            <Card.Text>{user.email}</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    )
+  );
+};
+
+export default User;
